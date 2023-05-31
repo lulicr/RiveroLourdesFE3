d8679a62f6dd1db277c104118ca84f7648ae500e
lulicr <luli_123123@hotmail.com>
2023-05-31 20:02:57 -0300

    ExamenCompleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nombre, setNombre] = useState('')
+  const [apellido, setApellido] = useState('')
+  const [grupo, setGrupo] =useState('')
+  const [fandom, setFandom] = useState('')
+  const [miembro, setMiembro] = useState('')
+
+  const [show, setShow] = useState(false)
+  const [error, setError] = useState(false)
+  
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if(nombre.length >= 3  && !nombre.startsWith(" ") 
+      && apellido.length >= 6 && grupo.length > 0 
+      && fandom.length > 0 && miembro.length > 0){
+    setShow(true)
+    setError(false)
+    } else {setError(true)}
+  }
 
   return (
     <>
       <div className="App">
-        <h1>Carga de estudiantes</h1>
-        <form></form>
-        <Card />
-    </div>
+
+        <h1 className='credencial'>¡Crea tu credencial!</h1>
+
+        <form onSubmit={handleSubmit} >
+          <label >Nombre</label>
+          <input type="text" disabled={show} onChange={(event)  => setNombre(event.target.value)}/>
+          <label >Apellido</label>
+          <input type="text" disabled={show} onChange={(event)  => setApellido(event.target.value)}/>
+          <label >Grupo Favorito</label>
+          <input type="text" disabled={show} onChange={(event)  => setGrupo(event.target.value)}/>
+          <label >Nombre del Fandom</label>
+          <input type="text" disabled={show} onChange={(event)  => setFandom(event.target.value)}/>
+          <label >Miembro favorito</label>
+          <input type="text" disabled={show} onChange={(event)  => setMiembro(event.target.value)}/>
+          <button type="submit">Enviar</button>
+        </form>
+
+        {show ?  
+        <>
+            <Card apellido={apellido} nombre={nombre} grupo={grupo} fandom={fandom} miembro={miembro} />
+        </>
+        :
+        null
+        }
+
+        
+          {error && 'Por favor chequea que la información sea correcta'}
+
+      </div>
     </>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Card.css' 
 
-const Card = () => {
+
+const Card = ({apellido, nombre, grupo, fandom, miembro}) => {
   return (
-    <div>Card</div>
+    <div className='cardStyle'>
+      <h1> {grupo} Credencial</h1>
+      <h2>Nombre:</h2>
+      <h3>{nombre} {apellido}</h3>
+      <h2>Grupo Favorito:</h2>
+      <h3>{grupo}</h3>
+      <h2>Fandom:</h2>
+      <h3>{fandom}</h3>
+      <h2>Miembro favorito:</h2>
+      <h3>{miembro}</h3>
+      </div>
+    
   )
 }
 
